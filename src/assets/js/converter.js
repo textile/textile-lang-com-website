dadec94700237eddcae51af53f420f1ae6006890
Phil Wareham <phil@designhive.com>
2018-11-23 14:30:51 +0000

    More JSHint fixes

--- src/assets/js/converter.js
+++ src/assets/js/converter.js
@@ -2,48 +2,38 @@
 //  Mr-Data-Converter
 //  Created by Shan Carter on 2010-09-01.
 
+//---------------------------------------
+// PUBLIC PROPERTIES
+//---------------------------------------
+
 function DataConverter(nodeId) {
-
-  //---------------------------------------
-  // PUBLIC PROPERTIES
-  //---------------------------------------
-
-  this.nodeId                 = nodeId;
-  this.node                   = $("#"+nodeId);
-
-  this.outputDataTypes        = [
+    this.nodeId                 = nodeId;
+    this.node                   = $("#"+nodeId);
+    this.outputDataTypes        = [
                                     {"text":"Textile",                "id":"txtable",          "notes":""},
                                     {"text":"HTML",                   "id":"html",             "notes":""}
                                 ];
-  this.outputDataType         = "txtable";
-
-  this.columnDelimiter        = "\t";
-  this.rowDelimiter           = "\n";
-
-  this.inputTextArea          = {};
-  this.outputTextArea         = {};
-
-  this.inputHeader            = {};
-  this.outputHeader           = {};
-  this.dataSelect             = {};
-
-  this.inputText              = "";
-  this.outputText             = "";
-
-  this.newLine                = "\n";
-  this.indent                 = "  ";
-
-  this.commentLine            = "//";
-  this.commentLineEnd         = "";
-  this.tableName              = "MrDataConverter";
-
-  this.useUnderscores         = true;
-  this.headersProvided        = true;
-  this.downcaseHeaders        = true;
-  this.upcaseHeaders          = false;
-  this.includeWhiteSpace      = true;
-  this.useTabsForIndent       = false;
-
+    this.outputDataType         = "txtable";
+    this.columnDelimiter        = "\t";
+    this.rowDelimiter           = "\n";
+    this.inputTextArea          = {};
+    this.outputTextArea         = {};
+    this.inputHeader            = {};
+    this.outputHeader           = {};
+    this.dataSelect             = {};
+    this.inputText              = "";
+    this.outputText             = "";
+    this.newLine                = "\n";
+    this.indent                 = "  ";
+    this.commentLine            = "//";
+    this.commentLineEnd         = "";
+    this.tableName              = "MrDataConverter";
+    this.useUnderscores         = true;
+    this.headersProvided        = true;
+    this.downcaseHeaders        = true;
+    this.upcaseHeaders          = false;
+    this.includeWhiteSpace      = true;
+    this.useTabsForIndent       = false;
 }
 
 //---------------------------------------
@@ -51,97 +41,87 @@ function DataConverter(nodeId) {
 //---------------------------------------
 
 DataConverter.prototype.create = function(w,h) {
-  var self = this;
+    var self = this;
 
-  //build HTML for converter
-  this.inputHeader = $('<div class="groupHeader" id="inputHeader"><p class="groupHeadline">Input CSV or tab-delimited data. <span class="subhead"> Using Excel? Simply copy and paste. No data on hand? <a href="#" id="insertSample">Use sample</a></span>.</p></div>');
-  this.inputTextArea = $('<textarea class="textInputs" id="dataInput"></textarea>');
-  var outputHeaderText = '<div class="groupHeader" id="inputHeader"><p class="groupHeadline">Output as <select name="Data Types" id="dataSelector" >';
+    // build HTML for converter
+    this.inputHeader = $('<div class="groupHeader" id="inputHeader"><p class="groupHeadline">Input CSV or tab-delimited data. <span class="subhead"> Using Excel? Simply copy and paste. No data on hand? <a href="#" id="insertSample">Use sample</a></span>.</p></div>');
+    this.inputTextArea = $('<textarea class="textInputs" id="dataInput"></textarea>');
+    var outputHeaderText = '<div class="groupHeader" id="inputHeader"><p class="groupHeadline">Output as <select name="Data Types" id="dataSelector" >';
+
     for (var i=0; i < this.outputDataTypes.length; i++) {
-
-      outputHeaderText += '<option value="'+this.outputDataTypes[i]["id"]+'" ' + (this.outputDataTypes[i]["id"] === this.outputDataType ? 'selected="selected"' : '') + '>' + this.outputDataTypes[i]["text"]+'</option>';
+        outputHeaderText += '<option value="'+this.outputDataTypes[i]["id"]+'" ' + (this.outputDataTypes[i]["id"] === this.outputDataType ? 'selected="selected"' : '') + '>' + this.outputDataTypes[i]["text"]+'</option>';
     }
+
     outputHeaderText += '</select><span class="subhead" id="outputNotes"></span></p></div>';
-  this.outputHeader = $(outputHeaderText);
-  this.outputTextArea = $('<textarea class="textInputs" id="dataOutput"></textarea>');
 
-  this.node.append(this.inputHeader);
-  this.node.append(this.inputTextArea);
-  this.node.append(this.outputHeader);
-  this.node.append(this.outputTextArea);
+    this.outputHeader = $(outputHeaderText);
+    this.outputTextArea = $('<textarea class="textInputs" id="dataOutput"></textarea>');
 
-  this.dataSelect = this.outputHeader.find("#dataSelector");
+    this.node.append(this.inputHeader);
+    this.node.append(this.inputTextArea);
+    this.node.append(this.outputHeader);
+    this.node.append(this.outputTextArea);
 
-  this.outputTextArea.click(function(evt){this.select();});
+    this.dataSelect = this.outputHeader.find("#dataSelector");
+    this.outputTextArea.click(function(evt){this.select();});
 
 
-  $("#insertSample").bind('click',function(evt){
-    evt.preventDefault();
-    self.insertSampleData();
-    self.convert();
-  });
+    $("#insertSample").bind('click',function(evt) {
+        evt.preventDefault();
+        self.insertSampleData();
+        self.convert();
+    });
 
-  $("#dataInput").keyup(function() {
-    self.convert();
-  });
+    $("#dataInput").keyup(function() {
+        self.convert();
+    });
 
-  $("#dataInput").change(function() {
-    self.convert();
-  });
+    $("#dataInput").change(function() {
+        self.convert();
+    });
 
-  $("#dataSelector").bind('change',function(evt){
+    $("#dataSelector").bind('change',function(evt) {
        self.outputDataType = $(this).val();
        self.convert();
-     });
+    });
 
-  this.resize(w,h);
+    this.resize(w,h);
 };
 
 DataConverter.prototype.resize = function(w,h) {
+    var paneWidth = w;
+    var paneHeight = (h-90)/2-20;
 
-  var paneWidth = w;
-  var paneHeight = (h-90)/2-20;
-
-  this.node.css({width:paneWidth});
-  this.inputTextArea.css({width:paneWidth-20,height:paneHeight});
-  this.outputTextArea.css({width: paneWidth-20, height:paneHeight});
-
+    this.node.css({width:paneWidth});
+    this.inputTextArea.css({width:paneWidth-20,height:paneHeight});
+    this.outputTextArea.css({width: paneWidth-20, height:paneHeight});
 };
 
 DataConverter.prototype.convert = function() {
+    this.inputText = this.inputTextArea.val();
+    this.outputText = "";
 
-  this.inputText = this.inputTextArea.val();
-  this.outputText = "";
+    //make sure there is input data before converting...
+    if (this.inputText.length > 0) {
+        if (this.includeWhiteSpace) {
+            this.newLine = "\n";
+        } else {
+            this.indent = "";
+            this.newLine = "";
+        }
 
+        CSVParser.resetLog();
+        var parseOutput = CSVParser.parse(this.inputText, this.headersProvided, this.delimiter, this.downcaseHeaders, this.upcaseHeaders);
+        var dataGrid = parseOutput.dataGrid;
+        var headerNames = parseOutput.headerNames;
+        var headerTypes = parseOutput.headerTypes;
+        var errors = parseOutput.errors;
 
-  //make sure there is input data before converting...
-  if (this.inputText.length > 0) {
-
-    if (this.includeWhiteSpace) {
-      this.newLine = "\n";
-      // console.log("yes")
-    } else {
-      this.indent = "";
-      this.newLine = "";
-      // console.log("no")
-    }
-
-    CSVParser.resetLog();
-    var parseOutput = CSVParser.parse(this.inputText, this.headersProvided, this.delimiter, this.downcaseHeaders, this.upcaseHeaders);
-
-    var dataGrid = parseOutput.dataGrid;
-    var headerNames = parseOutput.headerNames;
-    var headerTypes = parseOutput.headerTypes;
-    var errors = parseOutput.errors;
-
-    this.outputText = DataGridRenderer[this.outputDataType](dataGrid, headerNames, headerTypes, this.indent, this.newLine);
-
-
-    this.outputTextArea.val(errors + this.outputText);
-
-  } //end test for existence of input text
+        this.outputText = DataGridRenderer[this.outputDataType](dataGrid, headerNames, headerTypes, this.indent, this.newLine);
+        this.outputTextArea.val(errors + this.outputText);
+    } //end test for existence of input text
 };
 
 DataConverter.prototype.insertSampleData = function() {
-  this.inputTextArea.val("NAME\tVALUE\tCOLOR\tDATE\nAlan\t12\tblue\tSep. 25, 2009\nShan\t13\t\"green\tblue\"\tSep. 27, 2009\nJohn\t45\torange\tSep. 29, 2009\nMinna\t27\tteal\tSep. 30, 2009");
+    this.inputTextArea.val("NAME\tVALUE\tCOLOR\tDATE\nAlan\t12\tblue\tSep. 25, 2009\nShan\t13\t\"green\tblue\"\tSep. 27, 2009\nJohn\t45\torange\tSep. 29, 2009\nMinna\t27\tteal\tSep. 30, 2009");
 };
