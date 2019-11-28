d90b57852f040396237690a0553e488157adf915
philwareham <phil@designhive.com>
2019-11-28 10:17:21 +0000

    Move from Sass-lint to stylelint
    Sass-lint has been EOLed.

--- Gruntfile.js
+++ Gruntfile.js
@@ -189,12 +189,12 @@ module.exports = function (grunt)
             }
         },
 
-        // Validate Sass files via sass-lint.
-        sasslint: {
+        // Validate CSS files via stylelint.
+        stylelint: {
             options: {
-                configFile: '.sass-lint.yml'
+                configFile: '.stylelintrc.yml'
             },
-            target: ['<%= paths.src.sass %>**/*.scss']
+            src: ['<%= paths.src.sass %>**/*.{css,scss}']
         },
 
         // Uglify and copy JavaScript files from `node_modules` and from `src/js/` to `public/assets/js/`.
@@ -251,7 +251,7 @@ module.exports = function (grunt)
 
     // Register tasks.
     grunt.registerTask('build', ['clean', 'concurrent', 'replace', 'uglify']);
-    grunt.registerTask('css', ['sasslint', 'sass', 'postcss']);
+    grunt.registerTask('css', ['stylelint', 'sass', 'postcss']);
     grunt.registerTask('default', ['watch']);
     grunt.registerTask('travis', ['build']);
 };
